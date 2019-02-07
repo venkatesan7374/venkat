ab59f6633949425e1dc5b736c2fec510713ac2fe
venkatesan7374 <42862520+venkatesan7374@users.noreply.github.com>
2019-02-07 12:29:58 +0530

    Create leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,14 @@
+year =int(input(""))
+if (year % 4) == 0:
+   if (year % 100) ==):
+       if (year % 400) ==):
+           print("yes".format(year))
+       else:
+           print("no".format(year))
+    else:
+        print("yes".format(year))
+else:
+   print("no".format(year)) 
+        
+ 
+       
